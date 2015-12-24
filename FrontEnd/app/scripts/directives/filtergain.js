74493903f55b4df4c5b1eef66b610df8130342c7
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2015-12-24 15:45:23 +0100

    ajout de directives pour le filtre de gain et le filtre biquad

---
+++ FrontEnd/app/scripts/directives/filtergain.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name frontEndApp.directive:filterGain
+ * @description
+ * # filterGain
+ */
+angular.module('frontEndApp')
+  .directive('filterGain', function () {
+    return {
+        templateUrl: 'views/templates/filterGainTmpl.html',
+        restrict: 'E',
+        replace: true,
+        scope: {
+            filter : '='
+        }
+    };
+  });
