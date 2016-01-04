87d60e7a8dd8aa178c462e076b15fb682496c074
NablaT <remi.pourtier@laposte.net>
2016-01-04 15:52:06 +0100

    Directive postRender

--- FrontEnd/app/scripts/directives/postRender.js
+++ FrontEnd/app/scripts/directives/postRender.js
@@ -1,3 +1,20 @@
+'use strict';
+
 /**
- * Created by Remi on 04/01/2016.
+ * @ngdoc directive
+ * @name frontEndApp.directive:plumbMenuItem
+ * @description
+ * # plumbMenuItem
  */
+angular.module('frontEndApp')
+    .directive('postRender', [ '$timeout', function($timeout) {
+    var def = {
+        restrict : 'A',
+        terminal : true,
+        transclude : true,
+        link : function(scope, element, attrs) {
+            $timeout(scope.redraw, 0);  //Calling a scoped method
+        }
+    };
+    return def;
+}]);
