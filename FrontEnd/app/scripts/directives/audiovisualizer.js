4685a5627563c6dd403b6a1a41ab6f17db3e4c8a
Maxime Touroute <maxime.touroute@gmail.com>
2016-01-07 13:28:08 +0100

    Ajout d'un waveform visualiser et amelioration de l'egaliser visualiser

--- FrontEnd/app/scripts/directives/audiovisualizer.js
+++ FrontEnd/app/scripts/directives/audiovisualizer.js
@@ -11,8 +11,9 @@ angular.module('frontEndApp')
   .directive('audioVisualizer', function () {
     return {
       restrict: 'A',
-        scope: {filter: '=filter', context: '=context'},
+        scope: {analyzerNode: '='},
         controller: function($scope, $element, $timeout){
+
 
 
             // TODO : refaire au propre : les dimensions du canvas, la hauteur des traits... etc
@@ -20,36 +21,40 @@ angular.module('frontEndApp')
             // TODO : pouvoir modifier la fftsize, choses du genre
             // TODO : pouvoir choisir plusieurs visualisations
 
-
+            // The canvas context, used to draw stuff in the canvas
             var ctx = $element[0].getContext('2d');
 
-
-            // Meter gradient
-
-            var grd=ctx.createLinearGradient(0,0,0,10);
+            // gradient element for the meter
+            var meterColor = ctx.createLinearGradient(0,0,0,10);
             // Saturation value
-            grd.addColorStop(0,"red");
+            meterColor.addColorStop(0,"red");
             // Normal value
-            grd.addColorStop(1,"#9ec7ff");
-            ctx.fillStyle=grd;
+            meterColor.addColorStop(1,'rgb(42, 140, 252)');
+            var backgroundColor = 'rgb(230, 230, 230)';
+
 
             /**
              * The main rendering loop for a audiovisualizer
              */
             var draw = function()
             {
+                console.log("draw");
                 // Getting array of all frequency values
-                var array =  new Uint8Array($scope.filter.analyser.frequencyBinCount);
-                $scope.filter.analyser.getByteFrequencyData(array); // Utile.. pourquoi je sais pas encore
+                var array =  new Uint8Array($scope.analyzerNode.frequencyBinCount);
+                // Utile.. pourquoi je sais pas encore
+                $scope.analyzerNode.getByteFrequencyData(array);
 
                 // Cleaning previous visualisation
-                ctx.clearRect(0, 0, 2000, 2000);
+                ctx.fillStyle = backgroundColor;
+                ctx.fillRect(0, 0, 2000, 2000);
 
+                ctx.fillStyle = meterColor;
 
                 // TODO : ameliorer ça, prendre borne a borne, enfin bref faire un vrai truc propre
                 for (var i = 1 ; i < array.length; i+=6)
                 {
-                   // ctx.fillRect(i, 0, 4,array[i]);//, 4, ctx.height); // Equalizer depuis le haut du canvas
+                    // Equalizer depuis le haut du canvas
+                   // ctx.fillRect(i, 0, 4,array[i]);//, 4, ctx.height);
 
                     // Equalizer depuis le bas du canvas : valeur 100 pour la hauteur TODO mettre la hauteur du canvas
                     ctx.fillRect(i, 100-array[i]/2, 4,array[i]);//, 4, ctx.height);
@@ -66,4 +71,5 @@ angular.module('frontEndApp')
 
         }
     };
+
   });
---
+++ FrontEnd/app/scripts/directives/waveformvisualiser.js
@@ -0,0 +1,111 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name frontEndApp.directive:waveformVisualiser
+ * Directive to visualise a waveform (has to be in a CANVAS element)
+ * @description
+ * # waveformVisualiser
+ */
+angular.module('frontEndApp')
+  .directive('waveformVisualiser', function () {
+    return {
+        restrict: 'A',
+        scope: {buffer: '='},
+      controller: function ($scope, $element, $timeout) {
+
+
+
+        // The canvas context, used to draw stuff in the canvas
+        var ctx = $element[0].getContext('2d');
+
+
+          // FUCK YEAH
+          var canvasWidth = ctx.canvas.width;//100;//$element.width;
+          var canvasHeight = ctx.canvas.height;//100;//$element.height;
+
+        // STYLE ELEMENTS
+          ctx.lineWidth = 1;
+          ctx.strokeStyle = 'rgb(42, 140, 252)';
+          ctx.fillStyle = 'rgb(230, 230, 230)';
+
+
+        /**
+         * The main rendering loop for a drawing the waveform
+         */
+        var draw = function()
+        {
+           // If buffer has been initialized already
+            if($scope.buffer != null)
+            {
+                // Cleaning the canvas
+                ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+                // Waveform style
+
+                ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+
+                // Starting the drawing of a line
+                ctx.beginPath();
+
+
+                // Pixel size between two points
+                var sliceWidth = 1;
+
+                var x = 0;
+
+                // Getting a buffer channel (0 should always be available, coz there's at least one channel
+                var bufferData = $scope.buffer.getChannelData(0);
+
+                /*
+                console.log("buffer number of channels" +$scope.buffer.numberOfChannels);
+                console.log("bufferData element" +bufferData[800000]);
+                console.log("euhh" + bufferData[80000]*100)
+                console.log("bufferData length" +bufferData.length);
+                */
+
+
+                // step index to loop on the bufferData array
+                // chosen to have the full track inside the canvas
+                var bufferDataStep = parseInt( bufferData.length / ctx.canvas.width );
+
+                //console.log("bufferData index step" + bufferDataStep)
+
+
+                for(var i = 0; i < bufferData.length; i+= bufferDataStep) {
+
+
+                    // values between -1 and 1. We reput them between [-canvasHeight/2 ; canvasHeight/2]
+                    var v = (bufferData[i])*ctx.canvas.height/2;
+                    // "zero" point in the middle of the canvas
+                    var y = ctx.canvas.height/2 + v;
+
+                    // drawing "zero" point
+                    if(i == 0) {
+                        ctx.moveTo(x, ctx.canvas.height/2);
+
+                    } else {
+                        ctx.lineTo(x,y)
+                    }
+
+                    x += sliceWidth;
+                }
+                ctx.stroke();
+            }
+        };
+
+
+        // First call to draw
+        draw();
+
+
+
+          $scope.$watch('buffer', function(newValue)
+          {
+            console.log("waveformVisualiser : detected change in buffer !");
+              draw();
+          });
+      }
+    };
+  });
