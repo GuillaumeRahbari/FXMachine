9da2ed8687ce732d241ec5c17ba53b0ca1f479c2
NablaT <remi.pourtier@laposte.net>
2016-01-11 17:29:26 +0100

    sounds like a meeeerrrgeeee

--- FrontEnd/app/scripts/directives/connectable.js
+++ FrontEnd/app/scripts/directives/connectable.js
@@ -9,12 +9,15 @@
 angular.module('frontEndApp')
   .directive('connectable', function (JsPlumb) {
     return {
-      restrict: 'E',
+      restrict: 'A',
       link: function (scope, element, attrs) {
-        JsPlumb.ready(function(){
+        jsPlumb.ready(function(){
+          console.log("element: ",element);
           element.text('this is the connectable directive');
-          JsPlumb.makeTarget(element, {
+          jsPlumb.makeTarget(element, {
             anchor: 'Continuous',
+            endpoint:"Rectangle",
+            paintStyle:{ fillStyle:"gray" },
             maxConnections: 2
           })
         });
--- FrontEnd/app/scripts/directives/connector.js
+++ FrontEnd/app/scripts/directives/connector.js
@@ -9,12 +9,13 @@
 angular.module('frontEndApp')
   .directive('connector', function (JsPlumb) {
     return {
-      restrict: 'E',
+      restrict: 'A',
       link: function (scope, element, attrs) {
         element.text('this is the connector directive');
-        JsPlumb.setContainer("testplumb");
+        console.log("element: ",element);
+        jsPlumb.setContainer("testplumb");
         jsPlumb.connect({source:"element1", target:"element2"});
-        JsPlumb.makeSource(element, {
+        jsPlumb.makeSource(element, {
           anchor:"Continuous",
           maxConnections:2,
           endPoint:"Rectangle"
