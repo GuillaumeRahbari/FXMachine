11d75e47cfc6d45bd45e9168a5ac36e212ea88dd
Maxime Touroute <maxime.touroute@gmail.com>
2016-01-11 15:22:07 +0100

    visualisation filtres biquad et disto

--- FrontEnd/app/scripts/directives/audiofilters/filterbiquad.js
+++ FrontEnd/app/scripts/directives/audiofilters/filterbiquad.js
@@ -7,111 +7,121 @@
  * # filterBiquad
  */
 angular.module('frontEndApp')
-  .directive('filterBiquad', function () {
-      return {
-          templateUrl: '../../../views/templates/filterBiquadTmpl.html',
-          restrict: 'E',
-          replace: true,
-          scope: {
-              filter : '='
-          },
-          controller: function($scope, $element, $timeout)
-          {
-              console.log("controller biquad !");
-              var ctx = $element[0].querySelector('#biquadvisualisation').getContext('2d')
+    .directive('filterBiquad', function () {
+        return {
+            templateUrl: '../../../views/templates/filterBiquadTmpl.html',
+            restrict: 'E',
+            replace: true,
+            scope: {
+                filter : '='
+            },
+            controller: function($scope, $element, $timeout)
+            {
+                console.log("controller biquad !");
+                var ctx = $element[0].querySelector('#biquadvisualisation').getContext('2d');
 
-              var canvasWidth = ctx.canvas.width;
-              var canvasHeight = ctx.canvas.height;
+                var canvasWidth = ctx.canvas.width;
+                var canvasHeight = ctx.canvas.height;
 
-              // STYLE ELEMENTS
-              ctx.lineWidth = 1;
-             // ctx.strokeStyle = 'rgb(42, 140, 252)';
-              ctx.fillStyle = 'rgb(230, 230, 230)';
-              var textFillStyle =  'rgb(0, 0, 0)';
-              var backgroundFillStyle = 'rgb(230, 230, 230)';
-              var barFillStyle = 'rgb(42, 140, 252)';
-              var zeroValueFillStyle = 'rgb(200, 0, 0)';
+                // STYLE ELEMENTS
 
 
 
-
-
-              var draw = function()
-              {
-                  var frequencyArray = new Float32Array(40);
-                  for(var i = 0 ; i < frequencyArray.length ; i++)
-                  {
-                      frequencyArray[i] = i*500
-                  }
-
-                  var magResponseOutput = new Float32Array(40);
-                  var phaseResponseOutput = new Float32Array(40);
-
-                  $scope.filter.audioNode.getFrequencyResponse(frequencyArray,magResponseOutput,phaseResponseOutput);
-
-//lol
-
-                  ctx.fillStyle = backgroundFillStyle;
-                  ctx.fillRect(0,0,canvasWidth,canvasHeight);
-
-                  var widthDrawStep = canvasWidth / frequencyArray.length;
-
-                 // console.log("frequencyhArray length" + frequencyArray.length);
-                  ctx.font = "10px Arial";
+                var textFillStyle =  'rgb(0, 0, 0)';
+                var backgroundFillStyle = 'rgb(230, 230, 230)';
+                var frequencyLineStyle = 'rgb(255, 100, 100)';
+                var zeroValueFillStyle = 'rgb(0, 0, 0)';
 
 
 
-                  for(var i = 0; i <= frequencyArray.length-1;i++)
-                  {
+
+
+                var draw = function()
+                {
+                    // Setting up the array values we want, and the array containers
+                    var frequencyArray = new Float32Array(40);
+                    for(var i = 0 ; i < frequencyArray.length ; i++)
+                    {
+
+                        frequencyArray[i] = i*100
+                    }
+
+                    // Frequency response
+                    var magResponseOutput = new Float32Array(40);
+                    // Phase response
+                    var phaseResponseOutput = new Float32Array(40);
+
+                    // Getting frequency response
+                    $scope.filter.audioNode.getFrequencyResponse(frequencyArray,magResponseOutput,phaseResponseOutput);
+
+                    //******** DRAW !
+
+                    // ** Cleaning background
+                    ctx.fillStyle = backgroundFillStyle;
+
+                    ctx.fillRect(0,0,canvasWidth,canvasHeight);
+
+                    var widthDrawStep = canvasWidth / frequencyArray.length;
+
+                    // console.log("frequencyhArray length" + frequencyArray.length);
 
 
 
-                      ctx.fillStyle = barFillStyle;
-                      // magResponseOutput goes from 0 to 1
-                      var meterWidth = magResponseOutput[i]*(canvasHeight/2);//
-
-                      ctx.fillRect(i*widthDrawStep, canvasHeight - meterWidth, widthDrawStep, meterWidth);
 
 
+                    //** Drawing frequency response line
+                    ctx.strokeStyle = frequencyLineStyle;
+                    ctx.fillStyle = frequencyLineStyle;
+                    ctx.lineWidth=2;
+                    ctx.beginPath();
+                    ctx.moveTo(0,canvasHeight/2);
 
-                      //ctx.fillStyle = textFillStyle;
-                      //ctx.fillText(frequencyArray[i]+'Hz',i*widthDrawStep,canvasHeight-10);
-
-                      ctx.fillStyle = zeroValueFillStyle;
-                      ctx.moveTo(0,canvasHeight/2);
-                      ctx.lineTo(canvasWidth,canvasHeight/2);
-                      ctx.stroke();
-                      // listItem.innerHTML = '<strong>' + frequencyArray[i] + 'Hz</strong>: Magnitude ' + magResponseOutput[i] + ', Phase ' + phaseResponseOutput[i] + ' radians.';
-
-                  }
-
-
-                  ctx.fillStyle = textFillStyle;
-                  ctx.fillText(frequencyArray[0]+'Hz',0,canvasHeight-10);
-                  ctx.fillText(frequencyArray[frequencyArray.length-1]+'Hz',canvasWidth-20,canvasHeight-10);
+                    
+                    for(var i = 0; i <= frequencyArray.length-1;i++)
+                    {
+                        // magResponseOutput goes from 0 to 1. We rescale to half of size, to handle saturatio
+                        var meterHeight = magResponseOutput[i]*(canvasHeight/2);
+                        ctx.lineTo(i*widthDrawStep, canvasHeight - meterHeight);
+                        // listItem.innerHTML = '<strong>' + frequencyArray[i] + 'Hz</strong>: Magnitude ' + magResponseOutput[i] + ', Phase ' + phaseResponseOutput[i] + ' radians.';
+                    }
+                    ctx.stroke();
 
 
-              };
+                    // Zero dB line
+                    ctx.lineWidth = 1;
+                    ctx.strokeStyle = zeroValueFillStyle;
+                    ctx.fillStyle = zeroValueFillStyle;
+                    ctx.moveTo(0,canvasHeight/2);
+                    ctx.lineTo(canvasWidth,canvasHeight/2);
+                    ctx.stroke();
 
-              /*
-               Watching for a buffer change (like a new sound has been loaded
-               */
-              $scope.$watch('filter.audioNode.frequency.value', function(newValue)
-              {
-                  draw();
-              });
-              $scope.$watch('filter.audioNode.Q.value', function(newValue)
-              {
-                  draw();
-              });
-              $scope.$watch('filter.audioNode.gain.value', function(newValue)
-              {
-                  draw();
-              });
-              $scope.$watch('filter.audioNode.type', function(newValue)
-              {
-                  draw();
-              })
-          }
-      };
-  });
+                    // Drawing frequency legend
+                    ctx.fillStyle = textFillStyle;
+                    ctx.font = "10px Arial";
+                    ctx.fillText(frequencyArray[0]+'Hz',0,canvasHeight-10);
+                    ctx.fillText(frequencyArray[frequencyArray.length-1]+'Hz',canvasWidth-50,canvasHeight-10);
+
+                };
+
+                /*
+                 Watching for a buffer change (like a new sound has been loaded
+                 */
+                $scope.$watch('filter.audioNode.frequency.value', function(newValue)
+                {
+                    draw();
+                });
+                $scope.$watch('filter.audioNode.Q.value', function(newValue)
+                {
+                    draw();
+                });
+                $scope.$watch('filter.audioNode.gain.value', function(newValue)
+                {
+                    draw();
+                });
+                $scope.$watch('filter.audioNode.type', function(newValue)
+                {
+                    draw();
+                })
+            }
+        };
+    });
--- FrontEnd/app/scripts/directives/audiofilters/filterwaveshaper.js
+++ FrontEnd/app/scripts/directives/audiofilters/filterwaveshaper.js
@@ -16,9 +16,24 @@ angular.module('frontEndApp')
           filter : '=',
             distorsion : '='
         },
-          controller: function ($scope, $timeout) {
+          controller: function ($scope, $element, $timeout) {
 
               $scope.distorsion = 0;
+
+
+              var ctx = $element[0].querySelector('#distovisualisation').getContext('2d');
+
+              var canvasWidth = ctx.canvas.width;
+              var canvasHeight = ctx.canvas.height;
+
+              // STYLE ELEMENTS
+
+
+
+              var textFillStyle =  'rgb(0, 0, 0)';
+              var backgroundFillStyle = 'rgb(230, 230, 230)';
+              var frequencyLineStyle = 'rgb(255, 100, 100)';
+              var zeroValueFillStyle = 'rgb(0, 0, 0)';
 
 
               /**
@@ -27,7 +42,7 @@ angular.module('frontEndApp')
                * @returns {Float32Array} - the curve
                */
               var makeDistortionCurve = function(amount) {
-                  var k = typeof amount === 'number' ? amount : 50, n_samples = 44100,
+                  var k =  amount  , n_samples = 44100,
                       curve = new Float32Array(n_samples),
                       deg = Math.PI / 180,
                       i = 0,
@@ -35,10 +50,41 @@ angular.module('frontEndApp')
 
                   for ( ; i < n_samples; ++i ) {
                       x = i * 2 / n_samples - 1;
-                      curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+                      curve[i] = ( k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) ); // avant c'etait (3+k)
                   }
                   return curve;
               };
+
+
+              var drawCurve = function(curve)
+              {
+                  console.log("draw");
+                  // ** Cleaning background
+                  ctx.fillStyle = backgroundFillStyle;
+                  ctx.fillRect(0,0,canvasWidth,canvasHeight);
+
+                  var widthDrawStep = canvasWidth / curve.length;
+
+                  // console.log("frequencyhArray length" + frequencyArray.length);
+
+                  //** Drawing disto response line
+                  ctx.strokeStyle = frequencyLineStyle;
+                  ctx.fillStyle = frequencyLineStyle;
+                  ctx.lineWidth=2;
+                  ctx.beginPath();
+                  ctx.moveTo(0,canvasHeight/2);
+
+                   //   console.log("curve" + curve);
+                  for(var i = 0; i <= curve.length-1;i++)
+                  {
+                      // magResponseOutput goes from 0 to 10 for a max amount of 100
+                      var meterHeight = curve[i]*(canvasHeight/2);
+                      ctx.lineTo(i*widthDrawStep, canvasHeight/2 - meterHeight);
+              }
+                  ctx.stroke();
+
+              };
+
 
               /**
                * Updates the value of the audioNode
@@ -46,9 +92,11 @@ angular.module('frontEndApp')
               var update = function()
               {
                   console.log("update disto : " + $scope.distorsion);
-                   var curve = makeDistortionCurve($scope.distorsion);
+                   var curve = makeDistortionCurve($scope.distorsion+0);
 
                   $scope.filter.audioNode.curve = curve;
+
+                  drawCurve(curve);
               };
 
 
