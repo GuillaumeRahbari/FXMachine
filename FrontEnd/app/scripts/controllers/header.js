de8f69c15f58c6ccdb5b5f63b09cf6b550ca4d22
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2016-01-15 11:33:59 +0100

    manquait un truc

---
+++ FrontEnd/app/scripts/controllers/header.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontEndApp.controller:HeaderCtrl
+ * @description
+ * # HeaderCtrl
+ * Controller of the frontEndApp
+ */
+angular.module('frontEndApp')
+  .controller('HeaderCtrl', function (UserSrv, $location, $rootScope) {
+
+      this.logout = function () {
+          UserSrv.logout().then(
+              function (data) {
+                  $rootScope.header = 'default';
+                  $location.path('/');
+              },
+              function (error) {
+                  console.log(error);
+              }
+          )
+      }
+
+  });
