5188458224fe6406b9053f326d9d8bb2bc59bcf8
Quentin cornevin <qcornevin@gmail.com>
2016-01-21 11:35:06 +0100

    Router for the pedal information

--- BackEnd/domain/Pedal.js
+++ BackEnd/domain/Pedal.js
@@ -8,6 +8,8 @@ class Pedal {
         this._input = input;
         this._output = output;
         this._filters = filters;
+        this._rate = 0;
+        this._comments = [];
     }
 
     get filters() {
@@ -20,5 +22,21 @@ class Pedal {
 
     get output() {
         return this._output
+    }
+
+    get rate() {
+        return this._rate;
+    }
+
+    get comments() {
+        return this._comments;
+    }
+
+    pedalFromJSON(json, callback) {
+        // TODO wait for pedal
+    }
+
+    pedalToJSON(callback) {
+        // TODO : wait for pedal
     }
 }
