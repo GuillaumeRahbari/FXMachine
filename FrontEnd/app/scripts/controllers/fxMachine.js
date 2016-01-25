f43a95dd9d4d5681348180280b7b98ef2abe2059
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2016-01-25 17:25:50 +0100

    On peut connecter les filtres !!!

--- FrontEnd/app/scripts/controllers/fxMachine.js
+++ FrontEnd/app/scripts/controllers/fxMachine.js
@@ -10,80 +10,71 @@
  * @name frontEndApp.controller:fxMachineCtrl
  */
 angular.module('frontEndApp')
-    .controller('fxMachineCtrl',['$scope', 'Filter', 'Sound', 'WebAudio', 'Pedal', 'JsPlumb', function ($scope, Filter, Sound, WebAudio, Pedal,JsPlumb) {
+       .controller('fxMachineCtrl', ['$scope', 'Filter', 'Sound', 'WebAudio', 'Pedal', 'JsPlumb', 'WebAudioSrv', function ($scope, Filter, Sound, WebAudio, Pedal, JsPlumb, WebAudioSrv) {
 
-        var self = this;
+           var self = this;
 
-        var webaudio = new WebAudio();
-        this.webaudio = webaudio;
+           var webaudio  = WebAudioSrv.getMainWebAudio();
+           this.webaudio = webaudio;
 
-        var pedals = [];
-        this.pedals = pedals;
+           var pedals  = [];
+           this.pedals = pedals;
 
-        /**
-         * Add a pedal
-         */
-        self.addPedal = function()
-        {
-            console.log("add pedal!!");
-            pedals.push(new Pedal(webaudio));
-        };
+           /**
+            * Add a pedal
+            */
+           self.addPedal = function () {
+               console.log("add pedal!!");
+               pedals.push(new Pedal(webaudio));
+           };
 
-        /**
-         * This function removes a pedal from the pedal list.
-         * @param {Pedal} ped - The pedal to be removed.
-         */
-        self.removePedal = function(ped)
-        {
-            var index = this.pedals.indexOf(ped);
-            JsPlumb.remove(ped);
+           /**
+            * This function removes a pedal from the pedal list.
+            * @param {Pedal} ped - The pedal to be removed.
+            */
+           self.removePedal = function (ped) {
+               var index = this.pedals.indexOf(ped);
+               JsPlumb.remove(ped);
 
-            if (index > -1){
-                // removing the pedal
-                this.pedals.splice(index,1);
-                console.info("pedal removed");
-            }
-            else
-            {
-                console.warn("no pedal to remove");
-            }
-        };
+               if (index > -1) {
+                   // removing the pedal
+                   this.pedals.splice(index, 1);
+                   console.info("pedal removed");
+               }
+               else {
+                   console.warn("no pedal to remove");
+               }
+           };
 
-        /**
-         * Load pedal in webaudio service, and launch the audio
-         * @param ped
-         */
-        self.loadPedalToWebAudio = function(ped)
-        {
-            if(webaudio.isInitialized)
-            {
-                console.log("loading pedal");
+           /**
+            * Load pedal in webaudio service, and launch the audio
+            * @param ped
+            */
+           self.loadPedalToWebAudio = function (ped) {
+               if (webaudio.isInitialized) {
+                   console.log("loading pedal");
 
-                // TODO : temporaire. a terme, les connexions seront binded avec la vue du graph jsplumb, ou alors on fera la connexion manuellement ici
-                ped.cleanConnexions();
-                ped.connectFiltersInChain();
+                   // TODO : temporaire. a terme, les connexions seront binded avec la vue du graph jsplumb, ou alors on fera la connexion manuellement ici
+                   ped.cleanConnexions();
+                   ped.connectFiltersInChain();
 
-                console.log("send stuff to webaudio");
+                   console.log("send stuff to webaudio");
 
-                //****************************************
-                // la methode s'occupe elle meme de tout connecter ensuite
-                // We work with a copy of the array, just to be sure that above here, we won't alterate our beautiful pedal
-                // (main point of this architecture btw)
-                var filtersArray = ped.filters.slice();
-                filtersArray.push(ped.input); // Because we also need to connect input to the stuff
-                filtersArray.push(ped.output);
+                   //****************************************
+                   // la methode s'occupe elle meme de tout connecter ensuite
+                   // We work with a copy of the array, just to be sure that above here, we won't alterate our beautiful pedal
+                   // (main point of this architecture btw)
+                   var filtersArray = ped.filters.slice();
+                   filtersArray.push(ped.input); // Because we also need to connect input to the stuff
+                   filtersArray.push(ped.output);
 
-                webaudio.loadGraph(filtersArray, ped.input, ped.output);
-            }
-            else
-            {
-                console.warn("Webaudio not initialised yet, not loading any graph until it is");
-            }
+                   webaudio.loadGraph(filtersArray, ped.input, ped.output);
+               }
+               else {
+                   console.warn("Webaudio not initialised yet, not loading any graph until it is");
+               }
 
-        };
+           };
 
 
-
-
-
-    }]);
+       }]);
--- FrontEnd/app/scripts/directives/connectable.js
+++ FrontEnd/app/scripts/directives/connectable.js
@@ -5,20 +5,25 @@
  *
  * To use it you just need to add the attribute 'connectable' to an element.
  *
+ * The connectable corresponds to the 'in'.
+ *
  * @ngdoc directive
  * @name frontEndApp.directive:connectable
  * @description
  * # connectable
  */
 angular.module('frontEndApp')
-  .directive('connectable', function (JsPlumb) {
-    return {
-      restrict: 'A',
-      link: function (scope, element, attrs)  {
-        JsPlumb.makeTarget(element, {
-          anchor: 'Continuous',
-          endpoint:"Rectangle"
-        });
-      }
-    };
-  });
+       .directive('connectable', function (JsPlumb) {
+           return {
+               restrict: 'A',
+               scope   : {
+                   plumbCtrl: '='
+               },
+               link    : function (scope, element, attrs) {
+                   JsPlumb.makeTarget(element, {
+                       anchor  : 'Continuous',
+                       endpoint: "Rectangle"
+                   });
+               }
+           };
+       });
