93d2d3a10ca0db0e91138abf71c7a9c326d5589d
NablaT <remi.pourtier@laposte.net>
2016-01-30 09:50:34 +0100

    adding angular material

--- FrontEnd/test/karma.conf.js
+++ FrontEnd/test/karma.conf.js
@@ -31,6 +31,9 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/angular-aria/angular-aria.js',
+      'bower_components/angular-messages/angular-messages.js',
+      'bower_components/angular-material/angular-material.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
