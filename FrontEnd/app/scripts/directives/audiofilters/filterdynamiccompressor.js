346131aea22481812fd34c41121a33b5e35c9e2c
NablaT <remi.pourtier@laposte.net>
2016-02-01 11:30:41 +0100

    Bla bli blu

--- FrontEnd/app/scripts/directives/audiofilters/filterdynamiccompressor.js
+++ FrontEnd/app/scripts/directives/audiofilters/filterdynamiccompressor.js
@@ -17,12 +17,13 @@ angular.module('frontEndApp')
         },
           controller:function($scope){
               $scope.link=function(){
-                  console.log(" start scope filter", $scope.filter.audioNode.attack);
+                  console.log(" start scope filter", $scope.filter.audioNode.attack.value);
                   var tmp= $scope.filter.audioNode.attack.value;
                   tmp= Math.round(tmp*100)/100;
                   console.log("tmp: ", tmp);
                   $scope.tmp=tmp;
-                  $scope.filter.audioNode.attack.value=tmp;
+                  $scope.filter.audioNode.attack.setValueAtTime(tmp,0);
+                  //$scope.filter.audioNode.attack.value=tmp;
                   //$scope.filter.audioNode.attack.value*=0.01;
                   console.log(" end scope filter", $scope.filter.audioNode.attack.value);
               }
