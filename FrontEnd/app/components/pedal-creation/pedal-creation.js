b7f9f49bfde4e1b00cca1b7effc6998b0e3f7858
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2016-02-01 17:07:46 +0100

    Pedal creation component

---
+++ FrontEnd/app/components/pedal-creation/pedal-creation.js
@@ -0,0 +1,16 @@
+/**
+ * Created by guillaume on 01/02/2016.
+ */
+
+'use strict';
+
+/**
+ * @ngdoc component
+ * @name frontEndApp.component:pedalCreation
+ * @description
+ * # pedalCreation
+ */
+angular.module('frontEndApp')
+       .component('pedalCreation', {
+           templateUrl: '/components/pedal-creation/pedal-creation.html'
+       });
