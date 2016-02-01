e28969171ae7471e06f39f5b62a35a5e25b6b838
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2016-02-01 17:08:04 +0100

    pedal component

---
+++ FrontEnd/app/components/my-pedal/my-pedal.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name frontEndApp.directive:myPedal
+ * @description
+ * # myPedal
+ */
+angular.module('frontEndApp')
+       .component('myPedal', {
+           templateUrl: 'components/my-pedal/my-pedal.html'
+       });
