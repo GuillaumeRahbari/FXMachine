002ce61ef3f03af08a8fbd1dc04c445377e1268f
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2016-02-01 21:23:19 +0100

    Edition pédale ok

--- FrontEnd/app/scripts/app.js
+++ FrontEnd/app/scripts/app.js
@@ -9,60 +9,65 @@
  * Main module of the application.
  */
 angular
-  .module('frontEndApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/fxmachine', {
-        templateUrl: 'views/fxMachineV3.html',
-        controller: 'fxMachineCtrl',
-        controllerAs: 'fxmachine'
-      })
-      .when('/connection', {
-        templateUrl: 'views/connection.html',
-        controller: 'ConnectionCtrl',
-        controllerAs: 'connection'
-      })
-      .when('/profil', {
-        templateUrl: 'views/profil.html',
-        controller: 'ProfilCtrl',
-        controllerAs: 'profil'
-      })
-      .when('/pedal', {
-        templateUrl: 'views/pedal.html',
-        controller: 'PedalCtrl',
-        controllerAs: 'pedal'
-      })
-      .when('/home', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeCtrl',
-        controllerAs: 'home'
-      })
-      .otherwise({
-        redirectTo: '/home'
-      });
-  }
-  )
-
-.run(['$rootScope', '$cookies', function($rootScope, $cookies) {
-
-
-    $rootScope.$on( '$routeChangeStart', function() {
-        // On essaye de récupérer le cookie de connection.
-        if($cookies.get('userId') !== undefined) {
-            $rootScope.header = 'connected';
+    .module('frontEndApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+            $routeProvider
+                .when('/fxmachine', {
+                    templateUrl : 'views/fxMachineV3.html',
+                    controller  : 'fxMachineCtrl',
+                    controllerAs: 'fxmachine'
+                })
+                .when('/connection', {
+                    templateUrl : 'views/connection.html',
+                    controller  : 'ConnectionCtrl',
+                    controllerAs: 'connection'
+                })
+                .when('/profil', {
+                    templateUrl : 'views/profil.html',
+                    controller  : 'ProfilCtrl',
+                    controllerAs: 'profil'
+                })
+                .when('/pedal', {
+                    templateUrl : 'views/pedal.html',
+                    controller  : 'PedalCtrl',
+                    controllerAs: 'pedal'
+                })
+                .when('/home', {
+                    templateUrl : 'views/home.html',
+                    controller  : 'HomeCtrl',
+                    controllerAs: 'home'
+                })
+                .when('/pedal/:pedalId', {
+                    templateUrl : 'views/pedal.html',
+                    controller  : 'PedalCtrl',
+                    controllerAs: 'pedal'
+                })
+                .otherwise({
+                    redirectTo: '/home'
+                });
         }
-        // Sinon on propose le header où on peut se connecter.
-        else {
-            $rootScope.header = 'default';
+    )
+
+    .run(['$rootScope', '$cookies', function ($rootScope, $cookies) {
 
 
-        }
-    });
-}]);
+        $rootScope.$on('$routeChangeStart', function () {
+            // On essaye de récupérer le cookie de connection.
+            if ($cookies.get('userId') !== undefined) {
+                $rootScope.header = 'connected';
+            }
+            // Sinon on propose le header où on peut se connecter.
+            else {
+                $rootScope.header = 'default';
+
+
+            }
+        });
+    }]);
