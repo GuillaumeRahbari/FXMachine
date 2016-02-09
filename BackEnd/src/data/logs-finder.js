9c79b405e47cd26662ec1626bdf088c86b31eb31
Quentin cornevin <qcornevin@gmail.com>
2016-02-09 17:02:09 +0100

    Doc + clear

--- BackEnd/src/data/logs-finder.js
+++ BackEnd/src/data/logs-finder.js
@@ -1,14 +1,19 @@
 /**
+ * This module is used to find the information about the log in the collection "logs".
+ *
  * Created by Quentin on 1/25/2016.
  */
 
-var mongodb = require('mongodb');
-var url = 'mongodb://localhost:27017/FXMachine';
-var mongoClient = mongodb.MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 var core = require('../../app/core/core');
 
-
+/**
+ * This function allow you to find one log with the given logsId.
+ *
+ * @param       {uuid}          This logsId is the uuid of the log in mongodb.
+ * @param       {function}      This callback function take two parameter, the first one is the error message, null
+ *                              if there is no error, and the second one is the result, null if there is an error.
+ */
 function myfindOne(logsId, callback) {
     core.getDb(function(db) {
         var collection = db.collection('logs');
@@ -22,27 +27,6 @@ function myfindOne(logsId, callback) {
             }
         });
     });
-
-
-
-
-    /*
-    mongoClient.connect(url, function(err, db) {
-        if(err) {
-            console.log("Unable to connect to mongoDB", err);
-        } else {
-            var collection = db.collection('logs');
-            var o_id = new ObjectId(logsId);
-            collection.findOne({ _id : o_id}, function(err, result) {
-                if(err) {
-                    console.log(err);
-                    callback(err);
-                } else {
-                    callback(result);
-                }
-            })
-        }
-    }); */
 }
 
 
