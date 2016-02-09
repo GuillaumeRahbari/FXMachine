167ae38f5ef459d118ebfb4dd425ab99dfa2fc69
Quentin cornevin <qcornevin@gmail.com>
2016-02-09 17:07:54 +0100

    Doc + clear

--- BackEnd/src/data/pedal-finder.js
+++ BackEnd/src/data/pedal-finder.js
@@ -2,13 +2,15 @@
  * Created by Quentin on 1/14/2016.
  */
 
-var mongodb = require('mongodb');
-var url = 'mongodb://localhost:27017/FXMachine';
-var mongoClient = mongodb.MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 var core = require("../../app/core/core.js");
 
-
+/**
+ * This function allow you to find one pedal in the collection "pedals".
+ *
+ * @param pedalId
+ * @param callback
+ */
 function myfindOne(pedalId, callback) {
     core.getDb(function(db) {
         var collection = db.collection('pedals');
@@ -21,26 +23,6 @@ function myfindOne(pedalId, callback) {
             }
         });
     });
-
-
-
-
-    /*
-    mongoClient.connect(url, function(err, db) {
-        if(err) {
-            console.log("Unable to connect to mongoDB", err);
-        } else {
-            var collection = db.collection('pedals');
-            var o_id =  new mongodb.ObjectId(pedalId);
-            collection.find({_id: o_id }).toArray(function(error, result) {
-                if(error) {
-                    callback(err, null);
-                } else {
-                    callback(null, result);
-                }
-            })
-        }
-    }); */
 }
 
 exports.myfindOne = myfindOne;
